55a5ebbca9023ec2c74668aca2f2d46d8e41b57f
shk1003 <sgim02311@gmail.comgit config --global user.name shk1003>
2025-09-25 12:49:13 +0900

    과제풀이

---
+++ task/birthday/script/db.js
@@ -0,0 +1,64 @@
+const birthday_flower = [
+    {
+        month:1,
+        flower:'수선화',
+        content:'자기애, 자존심, 외로움',
+        src:'https://i.pinimg.com/736x/50/30/5e/50305e5f6f94704532d1dff5140237a8.jpg'
+    },{
+        month:2,
+        flower:'제비꽃',
+        content:'겸손, 양보',
+        src:'https://i.pinimg.com/736x/fe/2b/b3/fe2bb3e0007eec1808e1731327c95e0c.jpg'
+    },{
+        month:3,
+        flower:'수선화',
+        content:'자기애, 자존심, 외로움',
+        src:'https://i.pinimg.com/736x/50/30/5e/50305e5f6f94704532d1dff5140237a8.jpg'
+    },{
+        month:4,
+        flower:'스위트피',
+        content:'추억, 즐거움',
+        src:'https://i.pinimg.com/736x/98/41/bd/9841bd44603cf0d5dc96a69d74bc712a.jpg'
+    },{
+        month:5,
+        flower:'은방울꽃',
+        content:'희망, 섬세함',
+        src:'https://i.pinimg.com/736x/e9/45/f1/e945f17906933c4d273afb36f27f9ac9.jpg'
+    },{
+        month:6,
+        flower:'백합',
+        content:'순결',
+        src:'https://i.pinimg.com/736x/11/c3/87/11c38755af91e7ec719da215599f439a.jpg'
+    },{
+        month:7,
+        flower:'미나리아재비',
+        content:'아름다움, 인격',
+        src:'https://i.pinimg.com/736x/72/a8/bb/72a8bb38ff52d986098263eed3cbd48d.jpg'
+    },{
+        month:8,
+        flower:'글라디올러스',
+        content:'비밀, 상상, 견고함',
+        src:'https://i.pinimg.com/736x/76/cc/f5/76ccf584f527bf6b017b18b98060a624.jpg'
+    },{
+        month:9,
+        flower:'물망초',
+        content:'진실한 사랑',
+        src:'https://i.pinimg.com/736x/af/94/55/af9455a2d6932dbaf19ae47594cacf94.jpg'
+    },{
+        month:10,
+        flower:'금잔화',
+        content:'실망, 비애',
+        src:'https://i.pinimg.com/736x/94/c1/67/94c167e264f778c04e809acf3257e869.jpg'
+    },{
+        month:11,
+        flower:'국화',
+        content:'성실, 진실',
+        src:'https://i.pinimg.com/736x/c5/58/1d/c5581d797b84f0d78dd96cadaf3c744d.jpg'
+
+    },{
+        month:12,
+        flower:'포인세티아',
+        content:'축하, 감사',
+        src:'https://i.pinimg.com/736x/f6/3e/f5/f63ef5a5690eda5bb50f2ec05689cbdb.jpg'
+    }
+]
